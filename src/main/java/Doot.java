c6f6382778b8a63ff2865d0d7b9d0bc82174793f
Andy Cheong <rfcheong9292@gmail.com>
2025-01-28 19:24:43 +0800

    small test

--- src/main/java/Doot.java
+++ src/main/java/Doot.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
-
+//small test change
 
 public class Doot {
     static String[] formats = {
